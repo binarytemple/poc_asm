74f15248346520eef2f43f2b9d595bcafcb9f0f7
Bryan Hunt <irishjava@gmail.com>
2012-11-28 14:01:22 +0000

    refactor

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mvn install
+
 cat <<END
 **************************************************
 Executing the example jar
@@ -11,16 +13,16 @@ java -jar ./example/target/example-1.0-SNAPSHOT.jar
 
 cat <<END
 **************************************************
-Executing the example jar using the enhancer agent 
+Executing the example jar using the profiler agent
 **************************************************
 END
 
-java -javaagent:./enhancer/target/enhancer-1.0-SNAPSHOT.jar -jar ./example/target/example-1.0-SNAPSHOT.jar
+java -javaagent:./profiler/target/profiler-1.0-SNAPSHOT.jar -jar ./example/target/example-1.0-SNAPSHOT.jar
 
 cat <<END
 ********************************************************
-Executing the scala example jar using the enhancer agent 
+Executing the scala example jar using the profiler agent
 ********************************************************
 END
 
-java -javaagent:./enhancer/target/enhancer-1.0-SNAPSHOT.jar -jar ./scala-example/target/scala-example-1.0-SNAPSHOT.jar
+java -javaagent:./profiler/target/profiler-1.0-SNAPSHOT.jar -jar ./scala-example/target/scala-example-1.0-SNAPSHOT.jar
