ff22548f91a384a41f1412fa49cfe6d8671d2a0b
Bryan Hunt <sentimental.bryan@gmail.com>
2012-12-31 15:41:00 +0000

    last commit was messed up, this is the correct grammar and tests

--- config/src/test/java/AspectParserVisitingMain.java
+++ config/src/test/java/AspectParserVisitingMain.java
@@ -25,76 +25,76 @@ import java.util.Map;
 public class AspectParserVisitingMain {
 
 	public static void main(String args[]) throws IOException, ParseException {
-		java.io.InputStream is = null;
-		is = foo.Util.readFile("sample/valid-aspects1.conf");
-		AspectConfParser parser = new AspectConfParser(is);
-		SimpleNode n = parser.Start();
-		n.jjtAccept(new AspectConfParserVisitor() {
-
-			@Override
-			public Object visit(ASTMatcher node, Map data) {
-				System.err.println("visiting ASTMatcher :" + node.toString());
-				// TODO Auto-generated method stub
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTAlias node, Map data) {
-				System.err.println("visiting ASTAlias :" + node.jjtGetValue());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTMatchersSection node, Map data) {
-				System.err.println("visiting ASTMatchersSection"
-						+ node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTAliasSection node, Map data) {
-				System.err.println("visiting ASTAliasSection :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTExpression node, Map data) {
-				System.err.println("visiting ASTExpression :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTStart node, Map data) {
-				System.err.println("visiting ASTStart :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(SimpleNode node, Map data) {
-				System.err.println("visiting SimpleNode :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTMethodGlob node, Map data) {
-				System.err.println("visiting ASTMethodGlob :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTPath node, Map data) {
-				System.err.println("visiting ASTPath :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-
-			@Override
-			public Object visit(ASTFqcn node, Map data) {
-				System.err.println("visiting ASTFqcn :" + node.toString());
-				return node.childrenAccept(this, data);
-			}
-		}, new HashMap<Object, Object>());
-		n.dump("");
-		System.out.println("Thank you.");
-		is.close();
+//		java.io.InputStream is = null;
+//		is = foo.Util.readFile("sample/valid-aspects1.conf");
+//		AspectConfParser parser = new AspectConfParser(is);
+//		SimpleNode n = parser.Start();
+//		n.jjtAccept(new AspectConfParserVisitor() {
+//
+//			@Override
+//			public Object visit(ASTMatcher node, Map data) {
+//				System.err.println("visiting ASTMatcher :" + node.toString());
+//				// TODO Auto-generated method stub
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTAlias node, Map data) {
+//				System.err.println("visiting ASTAlias :" + node.jjtGetValue());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTMatchersSection node, Map data) {
+//				System.err.println("visiting ASTMatchersSection"
+//						+ node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTAliasSection node, Map data) {
+//				System.err.println("visiting ASTAliasSection :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTExpression node, Map data) {
+//				System.err.println("visiting ASTExpression :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTStart node, Map data) {
+//				System.err.println("visiting ASTStart :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(SimpleNode node, Map data) {
+//				System.err.println("visiting SimpleNode :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTMethodGlob node, Map data) {
+//				System.err.println("visiting ASTMethodGlob :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTPath node, Map data) {
+//				System.err.println("visiting ASTPath :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//
+//			@Override
+//			public Object visit(ASTFqcn node, Map data) {
+//				System.err.println("visiting ASTFqcn :" + node.toString());
+//				return node.childrenAccept(this, data);
+//			}
+//		}, new HashMap<Object, Object>());
+//		n.dump("");
+//		System.out.println("Thank you.");
+//		is.close();
 	}
 }
